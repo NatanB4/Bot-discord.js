aa69f93d1b0d5115745c561fa54aa7d736284775
natanbarbosa2 <natanbarbosa027@gmail.com>
2022-10-03 15:19:47 -0300

    #1 style: Format Files

--- src/Commands/Classification.js
+++ src/Commands/Classification.js
@@ -1,32 +1,33 @@
 const openai = require("../Config/OpenAi");
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require("discord.js");
 
 module.exports = class Opinion {
-    constructor() {
-        this.name = 'Classification'
-        this.description = 'Fala algo'
-        this.usage = 'say <mensagem>'
-        this.aliases = ['classifique', 'class', 'classification']
-    }
+  constructor() {
+    this.name = "Classification";
+    this.description = "Fala algo";
+    this.usage = "say <mensagem>";
+    this.aliases = ["classifique", "class", "classification"];
+  }
 
-    /**
-     * @param {import('discord.js').Message} message
-     * @param {string[]} args
-     */
-    async run(message, args) {
-        const completion =
-            await openai.createCompletion({
-                model: "text-davinci-002",
-                prompt: this.generatePrompt(args),
-                temperature: 0.6,
-                max_tokens: 200
-            });
+  /**
+   * @param {import('discord.js').Message} message
+   * @param {string[]} args
+   */
+  async run(message, args) {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: this.generatePrompt(args),
+      temperature: 0.6,
+      max_tokens: 200,
+    });
 
-        message.channel.send({ embeds: [this.messageEmbed(completion.data.choices[0].text)] });
-    }
+    message.channel.send({
+      embeds: [this.messageEmbed(completion.data.choices[0].text)],
+    });
+  }
 
-    generatePrompt(args) {
-        return `Classifique o sentimento da Frase
+  generatePrompt(args) {
+    return `Classifique o sentimento da Frase
         
         Frase: 'Eu amo cachorros'
         Resultado: 'Positivo'
@@ -37,24 +38,28 @@ module.exports = class Opinion {
         Frase: 'Ainda acho que você é um lixo'
         Resultado: 'Negativo'
 
-        Frase: ${args.join(' ')}
-        Resultado: `
+        Frase: ${args.join(" ")}
+        Resultado: `;
+  }
 
-    }
+  messageEmbed(description) {
+    const color = description.includes("Positivo")
+      ? "GREEN"
+      : description.includes("Negativo")
+      ? "RED"
+      : "YELLOW";
 
-    messageEmbed(description) {
-        const color = description.includes('Positivo') ? 'GREEN' : description.includes('Negativo') ? 'RED' : 'YELLOW';
+    const embed = new MessageEmbed()
+      .setTitle("Classificação de Sentimento")
+      .addFields({
+        name: "Resultado: ",
+        value: `#${description.trim()}`,
+        inline: true,
+      })
+      .setTimestamp()
+      .setColor(color);
 
-        const embed = new MessageEmbed()
-            .setTitle('Classificação de Sentimento')
-            .addFields(
-                { name: 'Resultado: ', value: `#${description.trim()}`, inline: true },
-            )
-            .setTimestamp()
-            .setColor(color)
-
-
-        return embed
-        // FILE gitignore
-    }
-}
+    return embed;
+    // FILE gitignore
+  }
+};
